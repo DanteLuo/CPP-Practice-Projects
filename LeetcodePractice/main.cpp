4d3ad876161962d39f96b19b98c0b0e08bf13feb
DanteLuo <luozongtai@hotmail.com>
2018-01-18 23:03:00 -0500

    Update main.cpp

--- LeetcodePractice/main.cpp
+++ LeetcodePractice/main.cpp
@@ -1,14 +1,13 @@
 #include "kthsmallmatrix.h"
 #include "Permutation.h"
 #include "NonOverlappingInt.h"
+#include "JumpGame.h"
 
 using namespace std;
 
 int
 main ()
 {
-  main_overlap();
+  main_jumpGame();
   return 0;
 }
-
-
